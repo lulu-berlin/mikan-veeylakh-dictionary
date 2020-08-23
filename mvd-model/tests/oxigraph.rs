5706cce69780b9469e9a6735383126035fceafc7
Ya'ar Hever <yaarhever@gmail.com>
2020-08-23 15:39:40 +0200

    Integrate oxigraph MemoryStore into tests
    - Remove SparqlResults JSON type

--- mvd-model/tests/oxigraph.rs
+++ mvd-model/tests/oxigraph.rs
@@ -1,10 +1,33 @@
 mod support;
 use oxigraph::sparql::Variable;
-use support::{oxigraph_docker::OxigraphContainer, sparql::SparqlDBClient};
+use support::{
+    oxigraph_docker::OxigraphContainer, oxigraph_memory_store::OxigraphMemoryStore,
+    sparql::SparqlDBClient,
+};
 
 #[test]
 fn start_oxigraph_docker() -> Result<(), Box<dyn std::error::Error>> {
     let oxigraph = OxigraphContainer::spawn();
+
+    let solutions = oxigraph.sparql_query(
+        r"SELECT *
+            WHERE { ?s ?p ?o }
+            LIMIT 10",
+    )?;
+
+    let variables = solutions.variables();
+
+    assert_eq!(variables.len(), 3);
+    assert!(variables.iter().any(|v| *v == Variable::new("s")));
+    assert!(variables.iter().any(|v| *v == Variable::new("p")));
+    assert!(variables.iter().any(|v| *v == Variable::new("o")));
+
+    Ok(())
+}
+
+#[test]
+fn start_oxigraph_memory_store() -> Result<(), Box<dyn std::error::Error>> {
+    let oxigraph = OxigraphMemoryStore::spawn();
 
     let solutions = oxigraph.sparql_query(
         r"SELECT *
